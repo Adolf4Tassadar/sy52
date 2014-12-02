f31e7f57fae975f2336185e86d5d7e5b27261967
wang <wolf@wangdeMacBook-Pro.local>
2014-12-02 13:53:08 +0800

    Initial Commit

---
+++ sy52/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sy52
+//
+//  Created by wang on 14-12-2.
+//  Copyright (c) 2014年 wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
